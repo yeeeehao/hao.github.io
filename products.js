63ef80d016a8440230858efe6c60c7add86d410a
769399481 <86551711+769399481@users.noreply.github.com>
2022-12-07 16:13:01 +0700

    Product Table

---
+++ products.js
@@ -0,0 +1,62 @@
+// define data in JSON array
+var products = [
+  {
+    name: "Samsung TV 50",
+    quantity: 1,
+    ppu: 15000,
+  },
+  {
+    name: "Xiaomi Fan",
+    quantity: 2,
+    ppu: 500,
+  },
+  {
+    name: "iPhone 14 Pro Max Ultra",
+    quantity: 1,
+    ppu: 50000,
+  },
+  {
+    name: "Logitec Gaming Mouse",
+    quantity: 1,
+    ppu: 4000,
+  },
+];
+
+function loadData() {
+  let productList = document.getElementById("productList");
+  let gross = 0;
+
+  document.getElementById("myName").innerHTML = "Hao Ye";
+  for (let p in products) {
+    let row = document.createElement("tr");
+    let productName = document.createElement("td");
+    productName.innerHTML = products[p].name;
+
+    let quantity = document.createElement("td");
+    quantity.innerHTML = products[p].quantity;
+    quantity.classList.add("text_center");
+
+    let ppu = document.createElement("td");
+    ppu.innerHTML = products[p].ppu;
+    ppu.classList.add("text_right");
+
+    let total = document.createElement("td");
+    total.innerHTML = products[p].ppu * products[p].quantity;
+    total.classList.add("text_right");
+    gross += products[p].ppu * products[p].quantity;
+
+    row.appendChild(productName);
+    row.appendChild(quantity);
+    row.appendChild(ppu);
+    row.appendChild(total);
+    productList.appendChild(row);
+  }
+
+  let grossElem = document.getElementById("gross");
+  grossElem.innerHTML = gross;
+
+  let vat = gross * 0.07;
+  let net = gross + vat;
+  document.getElementById("vat").innerHTML = vat.toFixed(2);
+  document.getElementById("net").innerHTML = net.toFixed(2);
+}
