2346825b941446dcfc72e33daee97b19be0c5bd7
769399481 <86551711+769399481@users.noreply.github.com>
2023-01-25 16:07:01 +0700

    blog

---
+++ component---src-pages-project-1-cs-js-b3b49916cb0017f5392d.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[864],{6042:function(e,t,r){r.r(t);var a=r(7294);t.default=function(){return a.createElement(a.Fragment,null,a.createElement("h1",{style:{margin:"3rem"}},"Grade Tracker for CS student"))}}}]);
+//# sourceMappingURL=component---src-pages-project-1-cs-js-b3b49916cb0017f5392d.js.map
---
+++ component---src-pages-project-1-it-js-0717a482c46d488d9a57.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[426],{5917:function(e,t,r){r.r(t);var a=r(7294);t.default=function(){return a.createElement("h1",{style:{margin:"3rem"}},"Grade Tracker for IT student")}}}]);
+//# sourceMappingURL=component---src-pages-project-1-it-js-0717a482c46d488d9a57.js.map
